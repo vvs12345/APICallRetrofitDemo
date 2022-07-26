d8bef01c1a8e9be279cb06591665dd80c2a0b1b8
vishalv <vishalv@meditab.com>
2022-07-26 13:25:31 +0530

    remove unnecessary classes, changed in api call code

--- app/src/main/java/com/example/apicallretrofitdemo/utils/NetworkExtensions.kt
+++ app/src/main/java/com/example/apicallretrofitdemo/utils/NetworkExtensions.kt
@@ -1,32 +1,44 @@
-//package com.example.apicallretrofitdemo.utils
-//
-//import java.io.IOException
-//
-//private const val SUCCESS = "1"
-//private const val RE_LOGIN = "-3"
-//
-//
-//
-//suspend fun <T> executeCoroutine(apiCall: suspend () -> T): NetworkResponseState<T> {
-//    return try {
-//        val response = apiCall.invoke()
-//        when (response.toString()) {
-//            SUCCESS -> NetworkResponseState.Success(response, response.toString())
-//            RE_LOGIN -> NetworkResponseState.TokenAuthFailed("response.description")
-//            else -> NetworkResponseState.OtherSuccess(response.hashCode(), "response.description", response)
-//        }
-//    } catch (e: Exception) {
-//        e.printStackTrace()
-//        when (e) {
-//            is retrofit2.HttpException -> {
-//                NetworkResponseState.Error(e.code(), "ERROR_NO_NETWORK")
-//            }
-//            is IOException -> {
-//                NetworkResponseState.Error(-2, "ERROR_NO_NETWORK")
-//            }
-//            else -> {
-//                NetworkResponseState.Error(-3, "ERROR_NO_NETWORK")
-//            }
-//        }
-//    }
-//}
+package com.example.apicallretrofitdemo.utils
+
+import com.example.apicallretrofitdemo.datamodels.ResponseObject
+import retrofit2.HttpException
+import java.io.IOException
+
+/**
+ * <h1>NehaM</h1>
+ *
+ * @author Neha Mehta (neham@meditab.com) Meditab Software Inc.
+ * @since 20/7/18 12:23 PM
+ *
+ */
+
+private const val ERROR_NO_NETWORK = "Unable to connect to server. Please contact Meditab support."
+private const val NETWORK_ERROR_CODE = 503
+private const val INTERNAL_SERVER_ERROR_CODE = 500
+private const val SUCCESS = "1"
+private const val RE_LOGIN = "-3"
+
+
+suspend fun <T> executeCoroutine(apiCall: suspend () -> ResponseObject<T>): CommentApiState<T> {
+    return try {
+        val response = apiCall.invoke()
+        when (response.code) {
+            SUCCESS -> CommentApiState.Success(response.data, response.description)
+            RE_LOGIN -> CommentApiState.TokenAuthFailed(response.description)
+            else -> CommentApiState.OtherSuccess(response.code.toInt(), response.description, response.data)
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        when (e) {
+            is HttpException -> {
+                CommentApiState.Error(e.code(), ERROR_NO_NETWORK)
+            }
+            is IOException -> {
+                CommentApiState.Error(NETWORK_ERROR_CODE, ERROR_NO_NETWORK)
+            }
+            else -> {
+                CommentApiState.Error(INTERNAL_SERVER_ERROR_CODE, ERROR_NO_NETWORK)
+            }
+        }
+    }
+}
